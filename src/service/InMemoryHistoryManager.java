44f15f6f519c73a79c439028a352cb983d8120a4
Daniil Andreev <daniiland9@yandex.ru>
2024-07-30 05:36:22 +0300

    Исправить замечания 2024.07.30

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -14,16 +14,17 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
+        if (task == null) return;
+        Node node;
         if (nodeStore.containsKey(task.id)) {
-            Node node = nodeStore.get(task.id);
+            node = nodeStore.get(task.id);
             node.task = task;
             removeNode(node);
-            smartLinkedList.linkLast(node);
         } else {
-            Node node = new Node(task);
-            smartLinkedList.linkLast(node);
+            node = new Node(task);
             nodeStore.put(task.id, node);
         }
+        smartLinkedList.linkLast(node);
     }
 
     @Override
@@ -33,54 +34,41 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void remove(int id) {
-        if (nodeStore.containsKey(id)) {
-            removeNode(nodeStore.get(id));
-            nodeStore.remove(id);
-        }
+        if (!nodeStore.containsKey(id)) return;
+        removeNode(nodeStore.get(id));
+        nodeStore.remove(id);
     }
 
     private void removeNode(Node node) {
-        if (node.equals(smartLinkedList.head)) {
-            if (node.equals(smartLinkedList.tail)) {
-                smartLinkedList.head = null;
-                smartLinkedList.tail = null;
-            } else {
-                Node nextNode = node.next;
-                nextNode.previous = null;
-                smartLinkedList.head = nextNode;
-            }
-        } else if (node.equals(smartLinkedList.tail)) {
-            Node prevNode = node.previous;
-            prevNode.next = null;
-            smartLinkedList.tail = prevNode;
-        } else {
-            Node prevNode = node.previous;
-            Node nextNode = node.next;
-            prevNode.next = nextNode;
-            nextNode.previous = prevNode;
-        }
+        Node prevNode = node.previous;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.previous = prevNode;
     }
 
     static class SmartLinkedList {
-        private Node head = null;
-        private Node tail = null;
+        private Node head;
+        private Node tail;
+
+        private SmartLinkedList() {
+            head = new Node(null);
+            tail = new Node(null);
+            head.next = tail;
+            tail.previous = head;
+        }
 
         private void linkLast(Node node) {
-            if (head == null) {
-                head = node;
-                tail = node;
-            } else {
-                node.previous = tail;
-                node.next = null;
-                tail.next = node;
-                tail = node;
-            }
+            Node prevTail = tail.previous;
+            prevTail.next = node;
+            node.previous = prevTail;
+            node.next = tail;
+            tail.previous = node;
         }
 
         private List<Task> getTasks() {
             List<Task> listTask = new ArrayList<>();
-            Node node = head;
-            while (node != null) {
+            Node node = head.next;
+            while (node != tail) {
                 listTask.add(node.task);
                 node = node.next;
             }
